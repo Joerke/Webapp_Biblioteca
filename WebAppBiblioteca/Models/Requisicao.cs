3719b5cb053524ad9c6536b2f00c75aad38b064a
Pablo Joerke <joerke08@outlook.com>
2017-09-13 19:33:52 -0400

    fase2

---
+++ WebAppBiblioteca/Models/Requisicao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppBiblioteca.Models
+{
+    public class Requisicao
+    {
+        public int RequisicaoId { get; set; }
+        public int LivroId { get; set; }
+        public int AlunoId { get; set; }
+        public DateTime DataRequisicao { get; set; }
+        public DateTime DataEntrega { get; set; }
+
+    }
+}
